84b0e3933b164121758c3fbfb7772a10ca447473
solerjuan <juandiegosolerp@gmail.com>
2018-09-24 22:05:49 +0200

    Including alignment measure (AM) as astrohog output

--- astrohog.py
+++ astrohog.py
@@ -13,6 +13,30 @@ from nose.tools import assert_equal, assert_true
 
 import matplotlib.pyplot as plt
 
+import collections
+import multiprocessing
+
+CorrMapPair = collections.namedtuple('CorrMapPair', [
+   'map1','map2',
+   'pos1','pos2', 
+   'pxsz','ksz','res',
+   'mask1','mask2',
+   'gradthres1','gradthres2',
+   'wd'
+])
+
+def process_item(item):
+   print('Process Item',item.pos1,item.pos2)
+   circstats, corrframe, sframe1, sframe2 = HOGcorr_frame(item.map1, item.map2, pxsz=item.pxsz, ksz=item.ksz, res=item.res, mask1=item.mask1, mask2=item.mask2, gradthres1=item.gradthres1, gradthres2=item.gradthres2, wd=item.wd)
+  
+   return {
+      'circstats': circstats,
+      'corrframe': corrframe, 
+      'sframe1': sframe1,
+      'sframe2': sframe2, 
+      'pos1': item.pos1,
+      'pos2': item.pos2
+   }
 
 # ------------------------------------------------------------------------------------------------------------------------
 def HOG_PRS(phi):
@@ -26,7 +50,7 @@ def HOG_PRS(phi):
    # s_Zx     - 
    # meanPhi  -
 
-   angles=2.*phi
+   angles=phi #2.*phi
 
    Zx=np.sum(np.cos(angles))/np.sqrt(np.size(angles)/2.)	
    temp=np.sum(np.cos(angles)*np.cos(angles))
@@ -40,6 +64,16 @@ def HOG_PRS(phi):
 
    return Zx, s_Zx, meanPhi  
 
+# ------------------------------------------------------------------------------------------------------------------------------
+def HOG_AM(phi):
+   # Calculate the alignment measure as introduced in Lazarian2007
+
+   angles=phi
+  
+   ami=2.*np.cos(phi)-1. 
+   am=np.mean(ami)
+
+   return am
 
 # -------------------------------------------------------------------------------------------------------------------------------
 def HOGvotes_simple(phi):
@@ -215,13 +249,17 @@ def HOGcorr_frame(frame1, frame2, gradthres1=0., gradthres2=0., pxsz=1., ksz=1.,
    can=circ.descriptive.mean(2.*phi[good], w=wghts)/2.
    pz, Z = circ.tests.rayleigh(2.*phi[good],  w=wghts)
    pv, V = circ.tests.vtest(2.*phi[good], 0., w=wghts)
- 
+
+   myV, s_myV, meanphi = HOG_PRS(2.*phi[good])
+
+   am = HOG_AM(phi[good])   
+
    #if (wd > 1):
    #   hogcorr, corrframe =HOGvotes_blocks(phi, wd=wd)
    #else:
    #   hogcorr, corrframe =HOGvotes_simple(phi)
   
-   circstats=[rvl, Z, V, pz, pv]
+   circstats=[rvl, Z, V, pz, pv, myV, s_myV, meanphi, am]
 
    return circstats, corrframe, smoothframe1, smoothframe2
    #return Zx, corrframe, smoothframe1
@@ -329,13 +367,13 @@ def HOGcorr_frameandvec(frame1, vecx, vecy, gradthres=0., vecthres=0., pxsz=1., 
 
 
 # ================================================================================================================
-def HOGcorr_cube(cube1, cube2, z1min, z1max, z2min, z2max, pxsz=1., ksz=1., res=1., mask1=0, mask2=0, wd=1, gradthres1=0., gradthres2=0., regrid=False, allow_huge=False):
+def HOGcorr_cube(cube1, cube2, z1min, z1max, z2min, z2max, pxsz=1., ksz=1., res=1., mask1=0, mask2=0, wd=1, gradthres1=0., gradthres2=0., regrid=False, allow_huge=False, multipro=False):
+
    # Calculates the correlation   
    #
    # INPUTS
    #
    # OUTPUTS
-   #
    #
 
    print('Computing HOG correlation')
@@ -353,47 +391,89 @@ def HOGcorr_cube(cube1, cube2, z1min, z1max, z2min, z2max, pxsz=1., ksz=1., res=
    pzplane=np.zeros([z1max+1-z1min, z2max+1-z2min])
    pvplane=np.zeros([z1max+1-z1min, z2max+1-z2min]) 
 
+   myvplane=np.zeros([z1max+1-z1min, z2max+1-z2min])
+   mys_vplane=np.zeros([z1max+1-z1min, z2max+1-z2min])
+   meanphiplane=np.zeros([z1max+1-z1min, z2max+1-z2min])
+   amplane=np.zeros([z1max+1-z1min, z2max+1-z2min]) 
+
    corrplane=np.zeros([z1max+1-z1min, z2max+1-z2min])
    corrframe=np.zeros([sz1[1],sz1[2]]) 
    scube1=np.zeros(sz1)
    scube2=np.zeros(sz2)
 
-   #if (regrid):
-   #   corrcube=np.zeros([sz1[0], np.int(np.round(sf*sz1[1]/pxres)), np.int(np.round(sf*sz1[2]/pxres))])       #np.zeros(sz1)
-   #   corrframe_temp=np.zeros([np.int(np.round(sf*sz1[1]/pxres)), np.int(np.round(sf*sz1[2]/pxres))]) #np.zeros([sz1[1],sz1[2]])
-   #   maskcube=np.zeros([sz1[0], np.int(np.round(sf*sz1[1]/pxres)), np.int(np.round(sf*sz1[2]/pxres))])       #np.zeros(sz1)
-   #else:
    corrcube=np.zeros([z1max+1-z1min, z2max+1-z2min,sz1[1],sz1[2]])           
    corrframe_temp=np.zeros([sz1[1],sz1[2]]) 
    maskcube=np.zeros(sz1) 
 
-   for i in range(z1min, z1max+1):
-      for k in range(z2min, z2max+1):
-         print(i-z1min,k-z2min)
-         frame1=cube1[i,:,:]
-         frame2=cube2[k,:,:]
-         if np.array_equal(np.shape(cube1), np.shape(mask1)):
-            if np.array_equal(np.shape(cube2), np.shape(mask2)):				
-               circstats, corrframe, sframe1, sframe2 = HOGcorr_frame(frame1, frame2, pxsz=pxsz, ksz=ksz, res=res, mask1=mask1[i,:,:], mask2=mask2[k,:,:], gradthres1=gradthres1, gradthres2=gradthres2, wd=wd, regrid=regrid, allow_huge=allow_huge)
+   if (multipro):
+
+      corrmappairs=(CorrMapPair(map1=np.zeros([sz1[1],sz1[2]]), map2=np.zeros([sz1[1],sz1[2]]), pos1=-9, pos2=-9, pxsz=pxsz, ksz=ksz, res=res, mask1=np.zeros([sz1[1],sz1[2]]), mask2=np.zeros([sz1[1],sz1[2]]), gradthres1=gradthres1, gradthres2=gradthres2, wd=wd),)
+      count=0
+   
+      for i in range(z1min, z1max+1):
+         for k in range(z2min, z2max+1):
+            oldcorrmappairs=corrmappairs
+
+            tempcorrmapair=CorrMapPair(map1=cube1[i,:,:], map2=cube2[k,:,:], pos1=i-z1min, pos2=k-z2min, pxsz=pxsz, ksz=ksz, res=res, mask1=mask1[i,:,:], mask2=mask2[k,:,:], gradthres1=gradthres1, gradthres2=gradthres2, wd=wd)   
+
+            corrmappairs=oldcorrmappairs+(tempcorrmapair,) 
+
+            del oldcorrmappairs
+            del tempcorrmapair
+ 
+            count+=1
+
+      pool = multiprocessing.Pool()
+      result = pool.map(process_item, corrmappairs) 
+
+      for i in range(1, count):
+         rplane[result[i]['pos1'],result[i]['pos2']] =result[i]['circstats'][0]
+         zplane[result[i]['pos1'],result[i]['pos2']] =result[i]['circstats'][1]
+         vplane[result[i]['pos1'],result[i]['pos2']] =result[i]['circstats'][2]
+         pzplane[result[i]['pos1'],result[i]['pos2']]=result[i]['circstats'][3]  
+         pvplane[result[i]['pos1'],result[i]['pos2']]=result[i]['circstats'][4]
+   
+         myvplane[result[i]['pos1'],result[i]['pos2']] =result[i]['circstats'][5] 
+         mys_vplane[result[i]['pos1'],result[i]['pos2']] =result[i]['circstats'][6]
+         meanphiplane[result[i]['pos1'],result[i]['pos2']] =result[i]['circstats'][7]
+         amplane[result[i]['pos1'],result[i]['pos2']] =result[i]['circstats'][8]
+
+         corrcube[result[i]['pos1'],result[i]['pos2'],:,:]=result[i]['corrframe']
+         scube1[result[i]['pos1']+z1min,:,:]=result[i]['sframe1']
+         scube2[result[i]['pos2']+z2min,:,:]=result[i]['sframe2']
+
+   else:
+
+      for i in range(z1min, z1max+1):
+         for k in range(z2min, z2max+1):  
+            print(i-z1min,k-z2min)
+            frame1=cube1[i,:,:]
+            frame2=cube2[k,:,:]
+            if np.array_equal(np.shape(cube1), np.shape(mask1)):
+               if np.array_equal(np.shape(cube2), np.shape(mask2)):				
+                  circstats, corrframe, sframe1, sframe2 = HOGcorr_frame(frame1, frame2, pxsz=pxsz, ksz=ksz, res=res, mask1=mask1[i,:,:], mask2=mask2[k,:,:], gradthres1=gradthres1, gradthres2=gradthres2, wd=wd, regrid=regrid, allow_huge=allow_huge)
+               else:
+                  circstats, corrframe, sframe1, sframe2 = HOGcorr_frame(frame1, frame2, pxsz=pxsz, ksz=ksz, res=res, mask1=mask1[i,:,:], gradthres1=gradthres1, gradthres2=gradthres2, wd=wd, regrid=regrid, allow_huge=allow_huge)
             else:
-               circstats, corrframe, sframe1, sframe2 = HOGcorr_frame(frame1, frame2, pxsz=pxsz, ksz=ksz, res=res, mask1=mask1[i,:,:], gradthres1=gradthres1, gradthres2=gradthres2, wd=wd, regrid=regrid, allow_huge=allow_huge)
-         else:
-            circstats, corrframe, sframe1, sframe2 = HOGcorr_frame(frame1, frame2, ksz=ksz, gradthres1=gradthres1, gradthres2=gradthres2, wd=wd, allow_huge=allow_huge)
+               circstats, corrframe, sframe1, sframe2 = HOGcorr_frame(frame1, frame2, ksz=ksz, gradthres1=gradthres1, gradthres2=gradthres2, wd=wd, allow_huge=allow_huge)
 
-         rplane[i-z1min,k-z2min]=circstats[0]
-         zplane[i-z1min,k-z2min]=circstats[1]
-         vplane[i-z1min,k-z2min]=circstats[2]
-         pzplane[i-z1min,k-z2min]=circstats[3]
-         pvplane[i-z1min,k-z2min]=circstats[4]                   
-         corrcube[i-z1min,k-z2min,:,:]=corrframe
-         scube2[k,:,:]=sframe2
+            rplane[i-z1min,k-z2min]=circstats[0]
+            zplane[i-z1min,k-z2min]=circstats[1]
+            vplane[i-z1min,k-z2min]=circstats[2]
+            pzplane[i-z1min,k-z2min]=circstats[3]
+            pvplane[i-z1min,k-z2min]=circstats[4]
+            
+            myvplane[i-z1min,k-z2min]=circstats[5]
+            mys_vplane[i-z1min,k-z2min]=circstats[6]
+            meanphiplane[i-z1min,k-z2min]=circstats[7]
+            amplane[i-z1min,k-z2min]=circstats[8]  
+              
+            corrcube[i-z1min,k-z2min,:,:]=corrframe
+            scube2[k,:,:]=sframe2
 
-      scube1[i,:,:]=sframe1
+         scube1[i,:,:]=sframe1 
 
-   #import pdb; pdb.set_trace() 
-
-   #return corrplane, corrcube, scube1, scube2
-   return [rplane,zplane,vplane,pzplane,pvplane], corrcube, scube1, scube2
+   return [rplane,zplane,vplane,pzplane,pvplane,myvplane,mys_vplane,meanphiplane,amplane], corrcube, scube1, scube2
 
 
 # ================================================================================================================
