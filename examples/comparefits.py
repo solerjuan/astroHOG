2461826b237577b94a4b53fd59ea18da8c1eb980
solerjuan <juandiegosolerp@gmail.com>
2018-09-27 05:40:37 +0200

    Example update

--- examples/comparefits.py
+++ examples/comparefits.py
@@ -7,10 +7,14 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-image1 = scipy.ndimage.imread('../data/image.001.png', flatten=True)
-image2 = scipy.ndimage.imread('../data/image.002.png', flatten=True)
+hdul=fits.open('../data/image1.fits')
+image1=hdul[0].data
+hdul.close()
+hdul=fits.open('../data/image2.fits')
+image2=hdul[0].data
+hdul.close()
 
-circstats, corrframe, smoothframe1, smoothframe2 = HOGcorr_frame(image1, image2, ksz=3.)
+circstats, corrframe, smoothframe1, smoothframe2 = HOGcorr_frame(image1, image2)
 
 print('Mean resultant vector (r)        ', circstats[0])
 print('Rayleigh statistic (Z)           ', circstats[1])
@@ -22,18 +26,21 @@ print('Alignment measure (AM)           ', circstats[8])
 hist, bin_edges = np.histogram(corrframe*180.0/np.pi, density=True, range=[-90.,90.], bins=40)
 bin_center=0.5*(bin_edges[0:np.size(bin_edges)-1]+bin_edges[1:np.size(bin_edges)])
 
-fig, ax = plt.subplots(2,3,figsize=(18.5, 10.5))
-import pdb; pdb.set_trace()
-ax[0,0].imshow(image1, cmap='Greys_r')
-ax[0,1].imshow(image2, cmap='Greys_r')
-im=ax[0,2].imshow(np.abs(corrframe)*180.0/np.pi, cmap='spring')
-cb1=plt.colorbar(im,fraction=0.046, pad=0.04)
+fig=plt.figure()
+ax1=plt.subplot(221)
+plt.imshow(image1, cmap='bone', origin='lower')
+ax1=plt.subplot(222)
+plt.imshow(image2, cmap='copper', origin='lower')
+ax1=plt.subplot(223)
+im=plt.imshow(np.abs(corrframe)*180.0/np.pi, cmap='spring', origin='lower')
+cb1=plt.colorbar(im) #,fraction=0.046, pad=0.04)
 cb1.set_label(r'$|\phi|$ [deg]')
-ax[1,1].step(bin_center, hist*100, color='red')
-ax[1,1].set_ylabel('Histogram density [%]')
-ax[1,1].set_xlabel(r'$\phi$ [deg]')
-ax[1,1].set_xticks([-90.,-45.,0.,45.,90.])
-#plt.tight_layout()
+ax1=plt.subplot(224)
+plt.step(bin_center, hist*100, color='red')
+plt.ylabel('Histogram density [%]')
+plt.xlabel(r'$\phi$ [deg]')
+plt.xticks([-90.,-45.,0.,45.,90.])
+plt.tight_layout()
 plt.show()
 
 #import pdb; pdb.set_trace()
