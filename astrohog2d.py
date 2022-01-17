2416f90fe0188da773308ddda410330c0f4b6024
Juan Soler <juandiegosolerp@gmail.com>
2022-01-17 15:22:36 +0100

    Updates on the astroHOG Montecarlo
    The input for the MC realizations can be a constant or a map with the same dimensions of the input images/cubes.

--- astrohog2d.py
+++ astrohog2d.py
@@ -78,12 +78,10 @@ def HOGcorr_ima(ima1, ima2, s_ima1=0., s_ima2=0., pxsz=1., ksz=1., res=1., nruns
    sz2=np.shape(ima2)
 
    mruns1=nruns
-   #if (s_ima1 == 0.):
    if np.all(s_ima1 == 0.):
       mruns1=1
 
    mruns2=nruns
-   #if (s_ima2 == 0.):
    if np.all(s_ima2 == 0.):
       mruns2=1
 
