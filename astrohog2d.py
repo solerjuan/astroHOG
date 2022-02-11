Masking in astrohog2d fixed
Raises warnings when masking all the pixels in one of the input maps.

astrohog2d.py
@@ -155,7 +155,7 @@ def HOGcorr_ima(ima1, ima2, s_ima1=0., s_ima2=0., pxsz=1., ksz=1., res=1., nruns
    return circstats, corrframe, sima1, sima2
 
 
-# --------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 def HOGcorr_imaLITE(ima1, ima2, pxsz=1., ksz=1., res=1., mode='nearest', mask1=0., mask2=0., gradthres1=0., gradthres2=0., weights=None):
    """ Calculates the spatial correlation between im1 and im2 using the HOG method 
 
@@ -167,7 +167,9 @@ def HOGcorr_imaLITE(ima1, ima2, pxsz=1., ksz=1., res=1., mode='nearest', mask1=0
    s_ima2 : 
    pxsz :
    ksz : Size of the derivative kernel in the pixel size units
-   
+   mode: Specify how the input array is extended when the kernel overlaps the border of the map. 
+         Default: 'nearest'; The input is extended by replicating the last pixel.   
+ 
    Returns
    -------
     hogcorr :  
@@ -178,17 +180,26 @@ def HOGcorr_imaLITE(ima1, ima2, pxsz=1., ksz=1., res=1., mode='nearest', mask1=0
 
    """
 
+   # Check if the images match
    assert ima2.shape == ima1.shape, "Dimensions of ima2 and ima1 must match"
    sz1=np.shape(ima1) 
+   # Assign weights if none are specified
    if weights is None:
       weights=np.ones(sz1)
+   # Assign weights if the weights are all the same
    if (np.size(weights)==1): 
       uniweights=weights
-      weights=uniweights*np.ones(sz1) 
+      weights=uniweights*np.ones(sz1)
+   # Check if the provided weights match the image
    assert weights.shape == ima1.shape, "Dimensions of weights and ima1 must match" 
+
+   # Check if the masks match the image shape
+   assert mask1.shape == ima1.shape, "Dimensions of mask1 and ima1 must match"
+   assert mask2.shape == ima2.shape, "Dimensions of mask2 and ima2 must match"
 
    pxksz=(ksz/(2*np.sqrt(2.*np.log(2.))))/pxsz #gaussian_filter takes sigma instead of FWHM as input
 
+   # Calculate gradients
    sima1=ndimage.filters.gaussian_filter(ima1, [pxksz, pxksz], order=[0,0], mode=mode)
    sima2=ndimage.filters.gaussian_filter(ima2, [pxksz, pxksz], order=[0,0], mode=mode)
    dI1dx=ndimage.filters.gaussian_filter(ima1, [pxksz, pxksz], order=[0,1], mode=mode)
@@ -207,12 +218,19 @@ def HOGcorr_imaLITE(ima1, ima2, pxsz=1., ksz=1., res=1., mode='nearest', mask1=0
    phi[bad]=np.nan
  
    # Excluding masked gradients
-   if np.array_equal(np.shape(ima1), np.shape(mask1)):
+   if (np.size((mask1 > 0.).nonzero()) > 1):
       m1bad=(mask1 < 1.).nonzero()
       phi[m1bad]=np.nan
-   if np.array_equal(np.shape(ima2), np.shape(mask2)):
+   else:
+      print("No unmasked elements in ima1")
+      phi[:]=np.nan
+
+   if (np.size((mask2 > 0.).nonzero()) > 1):
       m2bad=(mask2 < 1.).nonzero()
       phi[m2bad]=np.nan
+   else:
+      print("No unmasked elements in ima2")
+      phi[:]=np.nan
 
    good=np.isfinite(phi).nonzero()
    ngood=np.size(good)
@@ -233,6 +251,7 @@ def HOGcorr_imaLITE(ima1, ima2, pxsz=1., ksz=1., res=1., mode='nearest', mask1=0
       pear, peap = stats.pearsonr(sima1[good], sima2[good])
 
    else:
+      print("WARNING: not enough pixels to compute astroHOG")
       rvl=np.nan
       Z=np.nan; pz=np.nan;
       V=np.nan; pv=np.nan;

statests.py
@@ -3,9 +3,66 @@ astroHOG Statistical tests
 """
 
 import numpy as np
+from scipy.stats import circmean, circstd
 
 # ------------------------------------------------------------------------------------------------------------------------
-def HOG_PRS(phi, weights=None):
+def HOG_PRS(phi, weights=None, s_phi=None, nruns=1):
+   # Calculates the projected Rayleigh statistic of the distributions of angles phi.
+   #
+   # INPUTS
+   # phi      - angles between -pi/2 and pi/2
+   # weights  - statistical weights
+   #
+   # OUTPUTS
+   # Zx       - value of the projected Rayleigh statistic   
+   # s_Zx     - 
+   # meanPhi  -
+
+   if np.logical_or(s_phi is None, nruns<2):
+
+      output=HOG_PRSlite(phi, weights=weights)
+      return {'Z': output['Z'], 's_Z': output['s_Z'], 
+              'Zx': output['Zx'], 's_Zx': output['s_Zx'], 's_ZxMC': np.nan, 
+              'meanphi': output['meanphi'], 's_meanphi': np.nan, 
+              'mrv': output['mrv'], 's_mrv': np.nan}
+
+   else: 
+
+      arrZ=np.zeros(nruns)
+      arrs_Z=np.zeros(nruns)
+      arrZx=np.zeros(nruns)
+      arrs_Zx=np.zeros(nruns)   
+      arrmeanphi=np.zeros(nruns)
+      arrstdphi=np.zeros(nruns)
+      arrmrv=np.zeros(nruns)
+ 
+      for i in range(0,nruns):
+
+         inmaprand=np.random.normal(loc=phi, scale=s_phi)  
+         output=HOG_PRSlite(inmaprand, weights=weights)
+         arrZ[i]=output['Z']
+         arrs_Z[i]=output['s_Z']
+         arrZx[i]=output['Zx']
+         arrs_Zx[i]=output['s_Zx']
+         arrmeanphi[i]=output['meanphi']
+         arrstdphi[i]=output['stdphi']
+         arrmrv[i]=output['mrv']
+     
+      Z     =np.nanmean(arrZ)
+      s_Z   =np.nanmean(arrs_Z) 
+      Zx    =np.nanmean(arrZx)
+      s_Zx  =np.nanmean(arrs_Zx)
+      s_ZxMC=np.nanstd(arrZx) 
+      meanphi =circmean(arrmeanphi, low=-np.pi, high=np.pi)
+      s_meanphi=circstd(arrmeanphi, low=-np.pi, high=np.pi)  
+      mrv  =np.nanmean(arrmrv)
+      s_mrv=np.nanstd(arrmrv)
+     
+      return {'Z': Z, 's_Z': s_Z, 'Zx': Zx, 's_Zx': s_Zx, 's_ZxMC': s_ZxMC, 'meanphi': meanphi, 's_meanphi': s_meanphi, 'mrv': mrv, 's_mrv': s_mrv} 
+
+ 
+# ------------------------------------------------------------------------------------------------------------------------
+def HOG_PRSlite(phi, weights=None):
    # Calculates the projected Rayleigh statistic of the distributions of angles phi.
    #
    # INPUTS
@@ -36,10 +93,15 @@ def HOG_PRS(phi, weights=None):
    temp=np.sum(np.sin(angles)*np.sin(angles))
    s_Zy=np.sqrt((2.*temp-Zy*Zy)/np.size(angles))
 
-   meanPhi=0.5*np.arctan2(Zy, Zx)
+   Z=np.sqrt(Zx**2+Zy**2)
+   s_Z=np.sqrt(s_Zx**2+s_Zy**2)
 
+   meanphi=circmean(angles, low=-np.pi, high=np.pi)  #0.5*np.arctan2(Zy, Zx)
+   stdphi=circstd(angles, low=-np.pi, high=np.pi)
+
+   #import pdb; pdb.set_trace()
    #return Zx, s_Zx, meanPhi
-   return {'Zx': Zx, 's_Zx': s_Zx, 'meanphi': meanPhi, 'mrv': mrv}
+   return {'Z': Z, 's_Z': s_Z, 'Zx': Zx, 's_Zx': s_Zx, 'meanphi': meanphi, 'stdphi': stdphi,  'mrv': mrv}
 
 # ---------------------------------------------------------------------------------------------------------
 def HOG_AM(phi):