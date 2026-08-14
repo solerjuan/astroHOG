e475fcacf3527a69401b1ca5c46a2cfa54ddf3e1
Juan Soler <10508683+solerjuan@users.noreply.github.com>
2026-08-14 16:25:06 +0200

    Healpix tools update

--- astrohogHP.py
+++ astrohogHP.py
@@ -120,6 +120,153 @@ def gradienthp(hpmap, niter=3, ksz=None, w0=0., nsideout=None, ordering='ring'):
    return output
 
 # -------------------------------------------------------------------------------------
+def astroHOGhpStride(map1, map2, niter=3, ksz=3.0, gal_cut=0, vsize=np.rad2deg(hp.nside2resol(8)), ordering1='ring', ordering2='ring', mask1=None, mask2=None, computeVmap=True, weights=None, w1=0., w2=0., s_map1=None, s_map2=None):
+
+   nsidein1=hp.npix2nside(np.size(map1))
+   nsidein2=hp.npix2nside(np.size(map2))
+   nsidein=np.min([nsidein1,nsidein2])
+
+   if (mask1 is None):
+      mask1=np.ones_like(map1)
+   if (mask2 is None):
+      mask2=np.ones_like(map2)
+
+   if (nsidein1 > nsidein2):
+      mask1=hp.pixelfunc.ud_grade(mask1.copy(), nsidein)
+   if (nsidein1 < nsidein2):
+      mask2=hp.pixelfunc.ud_grade(mask2.copy(), nsidein)
+
+   lmax=int(180./ksz)
+   lmax0=hp.npix2nside(np.size(map1))
+
+   if (lmax > lmax0):
+      lmax=lmax0
+
+   if (weights is None):
+      weights=((hp.nside2resol(hp.npix2nside(np.size(map1)), arcmin=True)/60.)/ksz)**2
+
+   resbase=hp.nside2resol(hp.npix2nside(np.size(map1)), arcmin=True)/60.0
+   resHOG=hp.nside2resol(nsidein, arcmin=True)/60.0
+   pxksz=(ksz/resbase)/sigma2fwhm
+
+   # Gradient of map 1 ---------------------------------------------
+   output=gradienthp(map1, niter=niter, ksz=ksz, w0=w1, nsideout=nsidein, ordering=ordering1)
+   smap1=output['smap']
+   dmap1dtheta=output['dtheta']
+   dmap1dphi=output['dphi']
+   gradmap1=output['gradmap']
+
+   # Gradient of map 2 ------------------------------------------------------
+   output=gradienthp(map2, niter=niter, ksz=ksz, w0=w2, nsideout=nsidein, ordering=ordering2)
+   smap2=output['smap']
+   dmap2dtheta=output['dtheta']
+   dmap2dphi=output['dphi']
+   gradmap2=output['gradmap']
+
+   # Calculate relative orientation angles --------------------------------------------------------
+   cosalpha=(dmap1dtheta*dmap2dtheta+dmap1dphi*dmap2dphi)/(gradmap1*gradmap2)
+   sinalpha=(dmap1dtheta*dmap2dphi-dmap1dphi*dmap2dtheta)/(gradmap1*gradmap2)
+   alphao=np.arctan(sinalpha/cosalpha)
+   alphad=np.arctan2(sinalpha,cosalpha)
+
+   alphao[(mask1 < 1.).nonzero()]=np.nan;
+   alphao[(mask2 < 1.).nonzero()]=np.nan
+   alphad[(mask1 < 1.).nonzero()]=np.nan;
+   alphad[(mask2 < 1.).nonzero()]=np.nan
+
+   output=HOG_PRS(2.*alphao[np.isfinite(alphao).nonzero()], weights=weights)
+   Voall=output['Zx']
+   output=HOG_PRS(alphad[np.isfinite(alphad).nonzero()], weights=weights)
+   Vdall=output['Zx']
+
+   # Blocks ----------------------------------------------------------------------------------
+   nsidein=hp.npix2nside(np.size(alphad))
+   bookkeeping=np.zeros_like(alphad)
+
+   # -----------------------------------------------------------------
+   nangles=np.zeros(hp.nside2npix(nsidein))
+   Zomap=np.zeros(hp.nside2npix(nsidein))
+   Zdmap=np.zeros(hp.nside2npix(nsidein))
+
+   Vomap=np.zeros(hp.nside2npix(nsidein))
+   Vdmap=np.zeros(hp.nside2npix(nsidein))
+
+   VoMAXmap=np.zeros(hp.nside2npix(nsidein))
+   VdMAXmap=np.zeros(hp.nside2npix(nsidein))
+
+   meanmap1=np.zeros(hp.nside2npix(nsidein))
+   meanmap2=np.zeros(hp.nside2npix(nsidein))
+   stdmap1=np.zeros(hp.nside2npix(nsidein))
+   stdmap2=np.zeros(hp.nside2npix(nsidein))
+
+   npix=np.size(map1)
+   nside1=hp.npix2nside(npix)
+   indices=np.arange(0,npix,1)
+   vectors=np.asarray(hp.pix2vec(nsidein, indices)).T
+
+   npix2=np.size(map2)
+   nside2=hp.npix2nside(npix2)
+   radius=np.deg2rad(0.5*vsize) #hp.nside2resol(nsideout)
+
+   # ----------------------------
+   valid = np.isfinite(alphad)
+
+   cos_d = np.zeros_like(alphad)
+   sin_d = np.zeros_like(alphad)
+   cos_o = np.zeros_like(alphad)
+   sin_o = np.zeros_like(alphad)
+
+   cos_d[valid] = np.cos(alphad[valid])
+   sin_d[valid] = np.sin(alphad[valid])
+
+   # Axial orientation statistic
+   cos_o[valid]=np.cos(2.0*alphad[valid])
+   sin_o[valid]=np.sin(2.0*alphad[valid])
+
+   weight = float(np.ravel(weights)[0])  # Current default is scalar
+
+   for i in tqdm(indices):
+
+      selpix2=hp.query_disc(nside2, vectors[i,:], radius) 
+      meanmap2[i]=np.nanmean(map2[selpix2])
+      stdmap2[i]=np.nanstd(map2[selpix2])
+
+      selpix=hp.query_disc(nside1, vectors[i,:], radius)
+      meanmap1[i]=np.nanmean(map1[selpix])
+      stdmap1[i]=np.nanstd(map1[selpix])
+
+      # -----------------------------------------------------
+      goodpix = selpix[valid[selpix]]
+      ngood=goodpix.size
+      denominator=np.sqrt(ngood*weight/2.0)
+
+      cd=weight*np.sum(cos_d[goodpix])
+      sd=weight*np.sum(sin_d[goodpix])
+      co=weight*np.sum(cos_o[goodpix])
+      so=weight*np.sum(sin_o[goodpix])
+
+      Vdmap[i] = cd / denominator
+      Zdmap[i] = np.hypot(cd, sd) / denominator
+
+      Vomap[i] = co / denominator
+      Zomap[i] = np.hypot(co, so) / denominator
+
+      VdMAXmap[i]=np.sqrt(2.0*ngood*weight)
+      VoMAXmap[i]=VdMAXmap[i]
+      nangles[i]=ngood
+
+   circstats={'Vdall': Vdall, 'Voall': Voall, 'nmap': nangles,
+              'Vd': Vdmap, 'VdMAX': VdMAXmap,
+              'Vo': Vomap, 'VoMAX': VoMAXmap,
+              'alphad': alphad, 'alphao': alphao,
+              'meanmap1': meanmap1, 'meanmap2': meanmap2,
+              'stdmap1': stdmap1, 'stdmap2': stdmap2,
+              'gradmap1': gradmap1, 'gradmap2': gradmap2,
+              'smap1': smap1, 'smap2': smap2}
+
+   return circstats
+
+# -------------------------------------------------------------------------------------
 def astroHOGhplite(map1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, ordering1='ring', ordering2='ring', mask1=None, mask2=None, computeVmap=True, weights=None, w1=0., w2=0., s_map1=None, s_map2=None):
 
    nsidein1=hp.npix2nside(np.size(map1))
@@ -188,38 +335,32 @@ def astroHOGhplite(map1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, ordering
    bookkeeping=np.zeros_like(alphad)
 
    # -----------------------------------------------------------------
-   nangles=np.zeros(hp.nside2npix(nsidein))
-   Zomap=np.zeros(hp.nside2npix(nsidein))
-   Zdmap=np.zeros(hp.nside2npix(nsidein))
+   nangles=np.zeros(hp.nside2npix(nsideout))
+   Zomap=np.zeros(hp.nside2npix(nsideout))
+   Zdmap=np.zeros(hp.nside2npix(nsideout))
 
-   Vomap=np.zeros(hp.nside2npix(nsidein))
-   Vdmap=np.zeros(hp.nside2npix(nsidein))
+   Vomap=np.zeros(hp.nside2npix(nsideout))
+   Vdmap=np.zeros(hp.nside2npix(nsideout))
 
-   VoMAXmap=np.zeros(hp.nside2npix(nsidein))
-   VdMAXmap=np.zeros(hp.nside2npix(nsidein))
+   VoMAXmap=np.zeros(hp.nside2npix(nsideout))
+   VdMAXmap=np.zeros(hp.nside2npix(nsideout))
 
-   meanmap1=np.zeros(hp.nside2npix(nsidein))
-   meanmap2=np.zeros(hp.nside2npix(nsidein))
-   stdmap1=np.zeros(hp.nside2npix(nsidein))
-   stdmap2=np.zeros(hp.nside2npix(nsidein))
+   meanmap1=np.zeros(hp.nside2npix(nsideout))
+   meanmap2=np.zeros(hp.nside2npix(nsideout))
+   stdmap1=np.zeros(hp.nside2npix(nsideout))
+   stdmap2=np.zeros(hp.nside2npix(nsideout))
 
-   for i in tqdm(index0):
+   for i in index1:
+      
       dummy=np.zeros(hp.nside2npix(nsideout))
 
-      selpix2=hp.query_disc(hp.npix2nside(np.size(map2)), hp.pixelfunc.pix2vec(nsidein,i), 0.5*hp.nside2resol(nsideout))
-      meanmap2[i]=np.nanmean(map2[selpix2])
-      stdmap2[i]=np.nanstd(map2[selpix2])
+      selpix=hp.query_disc(nsidein, hp.pixelfunc.pix2vec(nsideout,i), 0.5*hp.nside2resol(nsideout))
 
-      selpix=hp.query_disc(hp.npix2nside(np.size(map1)), hp.pixelfunc.pix2vec(nsidein,i), 0.5*hp.nside2resol(nsideout))
-      l0, b0 = hp.vec2ang(np.array(hp.pixelfunc.pix2vec(nsidein,i)), lonlat=True)
       meanmap1[i]=np.nanmean(map1[selpix])
+      meanmap2[i]=np.nanmean(map2[selpix])
       stdmap1[i]=np.nanstd(map1[selpix])
-     
-      #dummy[selpix]=1.0
-      #hp.orthview(dummy, rot=[l0[0],b0[0]])
-      #plt.show()
+      stdmap2[i]=np.nanstd(map2[selpix])
 
-      # ------------------------------------------------------
       tempalphad=alphad[selpix]
       output=HOG_PRS(0.*tempalphad[np.isfinite(tempalphad).nonzero()], weights=weights)
       VdMAXmap[i]=output['Zx']
@@ -234,48 +375,6 @@ def astroHOGhplite(map1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, ordering
       Vomap[i]=output['Zx']
       output=HOG_PRS(np.zeros(np.size(tempalphad)), weights=weights)
       VoMAXmap[i]=output['Zx']
-
-   # -----------------------------------------------------------------
-   #nangles=np.zeros(hp.nside2npix(nsideout))
-   #Zomap=np.zeros(hp.nside2npix(nsideout))
-   #Zdmap=np.zeros(hp.nside2npix(nsideout))
-
-   #Vomap=np.zeros(hp.nside2npix(nsideout))
-   #Vdmap=np.zeros(hp.nside2npix(nsideout))
-
-   #VoMAXmap=np.zeros(hp.nside2npix(nsideout))
-   #VdMAXmap=np.zeros(hp.nside2npix(nsideout))
-
-   #meanmap1=np.zeros(hp.nside2npix(nsideout))
-   #meanmap2=np.zeros(hp.nside2npix(nsideout))
-   #stdmap1=np.zeros(hp.nside2npix(nsideout))
-   #stdmap2=np.zeros(hp.nside2npix(nsideout))
-
-   #for i in index1:
-      
-      #dummy=np.zeros(hp.nside2npix(nsideout))
-
-      #selpix=hp.query_disc(nsidein, hp.pixelfunc.pix2vec(nsideout,i), 0.5*hp.nside2resol(nsideout))
-
-      #meanmap1[i]=np.nanmean(map1[selpix])
-      #meanmap2[i]=np.nanmean(map2[selpix])
-      #stdmap1[i]=np.nanstd(map1[selpix])
-      #stdmap2[i]=np.nanstd(map2[selpix])
-
-      #tempalphad=alphad[selpix]
-      #output=HOG_PRS(0.*tempalphad[np.isfinite(tempalphad).nonzero()], weights=weights)
-      #VdMAXmap[i]=output['Zx']
-      #output=HOG_PRS(tempalphad[np.isfinite(tempalphad).nonzero()], weights=weights)
-      #nangles[i]=np.size(np.isfinite(tempalphad).nonzero())
-      #Zdmap[i]=output['Z']
-      #Vdmap[i]=output['Zx']
-
-      #tempalphao=alphao[selpix]
-      #output=HOG_PRS(2.*tempalphao[np.isfinite(tempalphao).nonzero()])
-      #Zomap[i]=output['Z']
-      #Vomap[i]=output['Zx']
-      #output=HOG_PRS(np.zeros(np.size(tempalphad)), weights=weights)
-      #VoMAXmap[i]=output['Zx']
 
    circstats={'Vdall': Vdall, 'Voall': Voall, 'nmap': nangles,
               'Vd': Vdmap, 'VdMAX': VdMAXmap,
@@ -295,23 +394,6 @@ def astroHOGhp(map1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, ordering1='r
    nsidein2=hp.npix2nside(np.size(map2))
    nsidein=np.min([nsidein1,nsidein2])
 
-   #hdu=fits.PrimaryHDU()
-   #hdu.header['NAXIS']=2
-   #hdu.header['NAXIS1']=int(resHOG/resbase)
-   #hdu.header['NAXIS2']=int(resHOG/resbase)
-   #hdu.header['CTYPE1']='GLON-TAN'
-   #hdu.header['CRPIX1']=hdu.header['NAXIS1']/2
-   #hdu.header['CRVAL1']=0.
-   #hdu.header['CDELT1']=-resbase
-   #hdu.header['CUNIT1']='deg     '
-   #hdu.header['CTYPE2']='GLAT-TAN'
-   #hdu.header['CRPIX2']=hdu.header['NAXIS2']/2
-   #hdu.header['CRVAL2']=0.
-   #hdu.header['CDELT2']=resbase
-   #hdu.header['CUNIT2']='deg     '
-   #hdu.header['COORDSYS']='Galactic'
-   #target_header=hdu.header.copy()
-
    Voall=np.nan; s_Voall=np.nan
    Vdall=np.nan; s_Vdall=np.nan
    Vomap=np.nan; s_Vomap=np.nan
@@ -328,17 +410,18 @@ def astroHOGhp(map1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, ordering1='r
  
       Voallvec=np.zeros(nruns)
       Vdallvec=np.zeros(nruns)
-      Vomapvec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      Vdmapvec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      VoMAXmapvec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      VdMAXmapvec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      meanmap1vec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      meanmap2vec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      stdmap1vec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      stdmap2vec=np.zeros([nruns,hp.nside2npix(nsidein)])
+      Vomapvec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      Vdmapvec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      VoMAXmapvec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      VdMAXmapvec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      meanmap1vec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      meanmap2vec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      stdmap1vec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      stdmap2vec=np.zeros([nruns,hp.nside2npix(nsideout)])
+      nanglesvec=np.zeros([nruns,hp.nside2npix(nsideout)])
+
       gradmap1vec=np.zeros([nruns,hp.nside2npix(nsidein)])
       gradmap2vec=np.zeros([nruns,hp.nside2npix(nsidein)])
-      nanglesvec=np.zeros([nruns,hp.nside2npix(nsidein)])
       alphaovec=np.zeros([nruns,hp.nside2npix(nsidein)])
       alphadvec=np.zeros([nruns,hp.nside2npix(nsidein)])
  
@@ -360,9 +443,9 @@ def astroHOGhp(map1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, ordering1='r
          meanmap2vec[i,:]=output['meanmap2']
          stdmap1vec[i,:]=output['stdmap1']
          stdmap2vec[i,:]=output['stdmap2']
+
          gradmap1vec[i,:]=output['gradmap1']
          gradmap2vec[i,:]=output['gradmap2']
-
          nanglesvec[i,:]=output['nmap']
          alphaovec[i,:]=output['alphao']
          alphadvec[i,:]=output['alphad'] 
@@ -414,9 +497,11 @@ def astroHOGhp(map1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, ordering1='r
       alphao=output['alphao']
       alphad=output['alphad']
  
-   circstats={'Vdall': Vdall, 'Voall': Voall, 'nmap': nangles,
-              'Vd': Vdmap, 'VdMAX': VdMAXmap,
-              'Vo': Vomap, 'VoMAX': VoMAXmap,
+   circstats={'Vdall': Vdall, 'Voall': Voall,
+              's_Vdall': s_Vdall, 's_Voall': s_Voall, 
+              'nmap': nangles,
+              'Vd': Vdmap, 's_Vd': s_Vdmap,'VdMAX': VdMAXmap,
+              'Vo': Vomap, 's_Vo': s_Vomap, 'VoMAX': VoMAXmap,
               'alphad': alphad, 'alphao': alphao, 
               'meanmap1': meanmap1, 'meanmap2': meanmap2,
               'stdmap1': stdmap1, 'stdmap2': stdmap2,
@@ -453,7 +538,6 @@ def astroHOGhpSamples(samples1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, o
    for i in range(0,nsamples):
 
       output=astroHOGhp(samples1[i,:], map2, niter=niter, ksz=ksz, gal_cut=gal_cut, nsideout=nsideout, ordering1=ordering1, ordering2=ordering2, mask1=mask1, mask2=mask2, computeVmap=computeVmap, weights=weights, w1=w1, w2=w2, s_map1=s_map1, s_map2=s_map2, nruns=nruns)
-
       Voallvec[i]=output['Voall']
       Vdallvec[i]=output['Vdall']
 
@@ -469,7 +553,7 @@ def astroHOGhpSamples(samples1, map2, niter=3, ksz=3.0, gal_cut=0, nsideout=8, o
       nanglesvec[i,:]=output['nmap']
       alphaovec[i,:]=output['alphao']
       alphadvec[i,:]=output['alphad']
-  
+ 
    Voall=np.mean(Voallvec)
    s_Voall=np.std(Voallvec)
    Vdall=np.mean(Vdallvec)
